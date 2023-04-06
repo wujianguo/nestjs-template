3c5acf6a407f8444aa202777ed130555ca3adde9
wujianguo <lsjustin89@gmail.com>
2023-04-06 11:19:26 +0800

    ci: fix deploy

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): { [key: string]: string } {
-    return { data: 'Hello World! 2' };
+    return { data: 'Hello World! 3' };
   }
 }
